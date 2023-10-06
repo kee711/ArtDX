093d03ed3eebfa9266630656c48ddf5858e64cab
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-10-07 00:38:47 +0900

    :art: Separates codes from main file

---
+++ lib/constants/test_data.dart
@@ -0,0 +1,46 @@
+class TestData {
+  static List<dynamic> localDataList = [
+    {
+      "id": 0,
+      "image": "../assets/job_cigarette_papers.jpeg",
+      "artist": "Alphonse Mucha",
+      "name": "Job Cigarette Papers",
+      "medium": "Lithograph",
+      "date": "1896",
+      "size": "59 x 173 cm",
+      "docent": "강유진 도슨트",
+      "keypoint":
+          " This poster features a modern girl with Pre-Raphaelite hair holding a lighted cigarette, symbolizing the contemporary femininity of the time.",
+      "description":
+          "Alphonse Mucha’s poster for Job cigarette papers has become an iconic work in design history. This poster features a modern girl with Pre-Raphaelite hair holding a lighted cigarette, symbolizing the contemporary femininity of the time. Mucha treated this poster as a canvas and broke down the conventional boundaries between art and craft. He incorporated intricate patterns, floral elements, and lettering to create a visually stunning piece that captures both the Art Nouveau style and the Bohemian spirit. \nThe ‘Mucha woman’ depicted in this poster established an iconic image that captured audiences worldwide. Her elaborate hair became one of Mucha’s trademarks, capturing the fantasy-like beauty he wished to convey to viewers. This design was also heavily influenced by Japonism, which is present in its flat two-dimensional forms and elongated figures. \nJob by Alphonse Mucha is not only an influential masterpiece from his famed career but also showcased his contribution to popular art movements like Art Nouveau at its height. The innovative use of ornamentation adds depth to this simple image of a lone smoker, transcending her into something more emblematic about what it means to be both human and female.",
+    },
+    {
+      "id": 1,
+      "image": "../assets/brillo_box.jpeg",
+      "artist": "Andy Warhol",
+      "name": "Brillo Box",
+      "medium": "Acrylic,silkscreen,plywood",
+      "date": "1964",
+      "size": "17 1/8 x 17 1/8 x 14 in",
+      "docent": "기민성 도슨트",
+      "keypoint":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac maximus massa. Ut tellus diam, scelerisque eget orci nec,",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac maximus massa. Ut tellus diam, scelerisque eget orci nec, elementum molestie urna. Nullam sed metus convallis, ullamcorper risus nec, vehicula est. Phasellus quis lacus ac nisi rhoncus consectetur sit amet eget justo. Pellentesque urna nunc, tempor vitae pretium eget, tincidunt at ante. Maecenas in dignissim felis. Morbi feugiat facilisis sodales. Fusce blandit quam mollis dui suscipit maximus. Maecenas in fermentum enim, et lobortis sem. Curabitur porttitor commodo tincidunt. Vestibulum vitae turpis maximus lacus placerat pulvinar. Donec vel neque eu felis fermentum tristique quis sed nulla.Cras rutrum rutrum tincidunt. Aliquam dapibus dui purus, vel mattis erat molestie eu. Vivamus bibendum hendrerit mauris in tristique. Cras nec eros at nisi congue volutpat non non ex. Integer efficitur nibh justo, et tempus dolor hendrerit id. Pellentesque facilisis orci a sem consequat, nec vehicula ante tempus. Praesent velit dolor, eleifend id magna nec, lobortis tempus eros. Curabitur non scelerisque ligula, quis eleifend mi. Morbi lobortis sollicitudin tellus, vitae viverra metus finibus sed. Proin et tristique libero. Pellentesque et urna et odio commodo pharetra non sed enim. Vestibulum suscipit nisl nisl, a cursus orci dapibus eu.Ut maximus orci orci, eu aliquam metus condimentum bibendum. Vivamus pulvinar dolor quis mauris convallis, ut tristique quam eleifend. Suspendisse consequat rhoncus dolor vel viverra. Morbi sed sapien non lacus ornare suscipit. Nunc tincidunt placerat pharetra. Vestibulum nec luctus neque, ac auctor sem. Fusce venenatis scelerisque nunc, vitae faucibus purus sodales sit amet. Proin pellentesque efficitur enim, vel hendrerit nisl faucibus quis. Nam consectetur nisi non placerat placerat. Vivamus congue tellus vitae pharetra consequat. Nam feugiat purus felis, ac blandit elit blandit eget. Curabitur at urna ac ante faucibus fringilla. Phasellus vitae nisi enim. Morbi elementum ligula in mollis sollicitudin. Nunc tristique, ex bibendum ultrices pretium, dui sem blandit elit, id rutrum nibh risus vel nulla.",
+    },
+    {
+      "id": 2,
+      "image": "../assets/the_star.jpg",
+      "artist": "Edgar Degas",
+      "name": "The Star",
+      "medium": "Pastel on paper",
+      "date": "1878",
+      "size": "60 x 44 cm",
+      "docent": "김동한 도슨트",
+      "keypoint":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac maximus massa. Ut tellus diam, scelerisque eget orci nec,",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac maximus massa. Ut tellus diam, scelerisque eget orci nec, elementum molestie urna. Nullam sed metus convallis, ullamcorper risus nec, vehicula est. Phasellus quis lacus ac nisi rhoncus consectetur sit amet eget justo. Pellentesque urna nunc, tempor vitae pretium eget, tincidunt at ante. Maecenas in dignissim felis. Morbi feugiat facilisis sodales. Fusce blandit quam mollis dui suscipit maximus. Maecenas in fermentum enim, et lobortis sem. Curabitur porttitor commodo tincidunt. Vestibulum vitae turpis maximus lacus placerat pulvinar. Donec vel neque eu felis fermentum tristique quis sed nulla.Cras rutrum rutrum tincidunt. Aliquam dapibus dui purus, vel mattis erat molestie eu. Vivamus bibendum hendrerit mauris in tristique. Cras nec eros at nisi congue volutpat non non ex. Integer efficitur nibh justo, et tempus dolor hendrerit id. Pellentesque facilisis orci a sem consequat, nec vehicula ante tempus. Praesent velit dolor, eleifend id magna nec, lobortis tempus eros. Curabitur non scelerisque ligula, quis eleifend mi. Morbi lobortis sollicitudin tellus, vitae viverra metus finibus sed. Proin et tristique libero. Pellentesque et urna et odio commodo pharetra non sed enim. Vestibulum suscipit nisl nisl, a cursus orci dapibus eu.Ut maximus orci orci, eu aliquam metus condimentum bibendum. Vivamus pulvinar dolor quis mauris convallis, ut tristique quam eleifend. Suspendisse consequat rhoncus dolor vel viverra. Morbi sed sapien non lacus ornare suscipit. Nunc tincidunt placerat pharetra. Vestibulum nec luctus neque, ac auctor sem. Fusce venenatis scelerisque nunc, vitae faucibus purus sodales sit amet. Proin pellentesque efficitur enim, vel hendrerit nisl faucibus quis. Nam consectetur nisi non placerat placerat. Vivamus congue tellus vitae pharetra consequat. Nam feugiat purus felis, ac blandit elit blandit eget. Curabitur at urna ac ante faucibus fringilla. Phasellus vitae nisi enim. Morbi elementum ligula in mollis sollicitudin. Nunc tristique, ex bibendum ultrices pretium, dui sem blandit elit, id rutrum nibh risus vel nulla.",
+    },
+  ];
+}
---
+++ lib/styles/palette.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// this class contain all the colors used in the app
+class Palette {
+  static const backgroundColor = Color(0xFFF2F0EF);
+  static const progressBar = Color(0xFFDCC1B3);
+  static const textBox = Color(0xFFF7F7F7);
+
+  static const black = Color(0xFF282828);
+  static const pointFont = Color(0xFF815E4E);
+
+  // gray colors
+  static const greyHalf = Color(0xFF484848);
+  static const grey1 = Color(0xFF565656);
+  static const grey2 = Color(0xFF767676);
+  static const grey3 = Color(0xFFABABAB);
+}
